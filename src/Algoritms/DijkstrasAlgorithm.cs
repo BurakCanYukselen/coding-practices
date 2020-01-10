ca2ef336bc598dc27868b737a915b5de966017a7
Burak Can Yükselen <burakcan.yukselen@gmail.com>
2020-01-10 14:57:45 +0300

    DijkstrasAlgorithmTest

---
+++ src/Algoritms/DijkstrasAlgorithm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoritms
+{
+    public class DijkstrasAlgorithm
+    {
+        public int Dijkstras(int[,] map, int from, int to)
+        {
+            var adjNodes = new Dictionary<int, int>();
+            adjNodes.Add(from, 0);
+            var travelledNodes = new List<int>();
+
+            while (from != to)
+            {
+                travelledNodes.Add(from);
+
+                for (var i = 0; i < map.GetLength(1); i++)
+                {
+                    if (map[from, i] == 0 || travelledNodes.Contains(i))
+                        continue;
+
+                    var nextNodePathValue = map[from, i];
+                    var nextNodePathValueSum = adjNodes[from] + nextNodePathValue;
+
+                    if (adjNodes.ContainsKey(i))
+                    {
+                        if (nextNodePathValueSum >= adjNodes[i])
+                            continue;
+                        else
+                            adjNodes[i] = nextNodePathValueSum;
+                    }
+                    else
+                        adjNodes.Add(i, nextNodePathValueSum);
+                }
+
+                var minValueOfTravelledNode = -1;
+                foreach (var node in adjNodes)
+                {
+                    if (travelledNodes.Contains(node.Key))
+                        continue;
+                    if (minValueOfTravelledNode < 0 || minValueOfTravelledNode > node.Value)
+                    {
+                        minValueOfTravelledNode = node.Value;
+                        from = node.Key;
+                    }
+                }
+            }
+
+            var min = adjNodes[from];
+
+            return min;
+        }
+    }
+}
---
+++ src/Test/DijkstrasAlgorithmTest.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Runtime.CompilerServices;
+using Algoritms;
+using Xunit;
+
+namespace Test
+{
+    public class DijkstrasAlgorithmTest
+    {
+        public static int[,] Graph => new int[,]
+        {
+            //A, B, C,  D,  E, F, G
+            { 0, 4, 3, 0, 7, 0, 0 }, // A 
+            { 4, 0, 6, 5, 0, 0, 0 }, // B 
+            { 3, 6, 0, 11, 8, 0, 0 }, // C 
+            { 0, 5, 11, 0, 2, 2, 10 }, // D 
+            { 7, 0, 8, 2, 0, 0, 5 }, // E 
+            { 0, 0, 0, 2, 0, 0, 3 }, // F 
+            { 0, 0, 0, 10, 5, 3, 0 }, // G 
+        };
+
+        [Theory]
+        [InlineData(0, 5, 11)] // A to F
+        [InlineData(1, 6, 10)] // A to F
+        [InlineData(2, 5, 12)] // A to F
+        [InlineData(5, 4, 4)] // A to F
+        public void Test(int from, int to, int expected)
+        {
+            // Arrange
+            var algo = new DijkstrasAlgorithm();
+
+            // Act
+            var sut = algo.Dijkstras(Graph, from, to);
+
+            // Assert
+            Assert.Equal(expected, sut);
+        }
+    }
+}
